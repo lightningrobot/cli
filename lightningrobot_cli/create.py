d791904d5d73f26cce1b463b500298e82bcf052b
MeowSauce <meowsauce@hotmail.com>
2024-03-16 22:25:26 +0800

    删除 无用代码。

--- lightningrobot_cli/create.py
+++ lightningrobot_cli/create.py
@@ -22,14 +22,6 @@ def main():
     # 修改解压后的目录名称为用户提供的项目名称
     os.rename("template-main", name)
 
-    # 替换文本内容：读取并修改 pyproject.toml 文件
-    search_text = "template"
-    with open(r'pyproject.toml', 'r', encoding='UTF-8') as file:
-        data = file.read()
-        data = data.replace(search_text, name)
-    with open(r'pyproject.toml', 'w', encoding='UTF-8') as file:
-        file.write(data)
-
     # 删除 ZIP 文件
     os.remove('template.zip')
     # 打印创建成功消息
@@ -41,13 +33,8 @@ def main():
     adapter_package = f"lightningrobot-adapter-{adaptername}"
     # 使用 pip 安装指定适配器
     pip._internal.main(['install', adapter_package])
-    # 获取适配器配置信息
-    adapter_url = install.get_package_config(adapter_package)
     # 打印安装成功消息
     print(f"[信息]成功安装适配器包 {adapter_package}！（来源：PyPI）")
-    # 创建适配器所在路径
-    path = f"{name}/adapters/{adaptername}"
-    os.makedirs(path, exist_ok=True)  # 添加exist_ok=True以避免已存在时抛出异常
     # 打印创建路径成功的消息
     print("[信息]创建成功！")    
 
